81a5b2c4b7e64d1adb56e81edf211331852b8b15
Max <addergtx@gmail.com>
2020-10-07 21:53:06 +0300

    ready for hosting

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express()
 
@@ -9,6 +10,19 @@ app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth.routes'))
 //роутеры для работы с сылками
 app.use('/api/link', require('./routes/link.routes'))
+//роутер для перехода по ссылке
+app.use('/t', require('./routes/redirect.routes'))
+
+if (process.env.NODE_ENV === 'production') {
+    //сформировали статическую папку
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+    //любой get запрос будем отправлять 
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))        
+        //таким образом у нас будет работать и бэк и фронт одновременно
+        //нода будет за фсё отвечать
+    })
+}
 
 const PORT = config.get('port') || 5000
 
